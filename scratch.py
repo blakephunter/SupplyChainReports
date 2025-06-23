7ae766e284201d48be79f80f78e0445b3695b5f2
blakephunter <blake.hunter@dometic.com>
2025-06-23 12:07:46 -0400

    Weeks OnHand.py

--- scratch.py
+++ scratch.py
@@ -1,11 +1,26 @@
-# app.py
 import streamlit as st
+import pandas as pd
+import sqlalchemy
 
-def main():
-    st.title("Welcome App")
-    name = st.text_input("Enter your name")
-    if name:
-        st.write(f"Hello, {name}!")
+file_date = pd.Timestamp.today().strftime('_%Y%m%d%H%M%S')
+username = 'D365FO'
+password = 'g3Vj0dbiKS8Mhqz7lLbt'
+server = 'az06gl-prd-azsql01.database.windows.net'
+database = 'D365FO_Operations_Prod'
 
-if __name__ == "__main__":
-    main()
+conn_str = (
+    f"mssql+pyodbc://{username}:{password}@{server}/{database}"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
+)
+
+query = "select * from dbo.DIO_BH_WeeksOnHandTbl"
+engine = sqlalchemy.create_engine(conn_str)
+df = pd.read_sql_query(query,engine)
+df_tofile = df.to_excel(sheet_name='WOH')
+
+st.download_button(
+    label="Download Weeks On-Hand Report",
+    data=df_tofile,
+    file_name=f'WeeksOnHand{file_date}.xlsx',
+    mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+)
